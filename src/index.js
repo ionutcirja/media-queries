a1a5b481d3f9bfa413f9b25cd1c7f95948fdef40
Ionut Cirja <ionut.cirja@gmail.com>
2017-04-24 10:10:59 +0100

    lint

--- src/index.js
+++ src/index.js
@@ -1,43 +1,43 @@
 const mqList = {};
 
 function addBreakPointListener(bp, cb) {
-	if (mqList[bp]) {
-		mqList[bp].addListener(cb);
-	}
+    if (mqList[bp]) {
+        mqList[bp].addListener(cb);
+    }
 }
 
 function removeBreakPointListener(bp, cb) {
-	if (mqList[bp]) {
-		mqList[bp].removeListener(cb);
-	}
+    if (mqList[bp]) {
+        mqList[bp].removeListener(cb);
+    }
 }
 
 export function initMediaQueries(breakpoints) {
-	Object.keys(breakpoints).forEach((key) => {
-		mqList[key] = window.matchMedia(`(min-width: ${breakpoints[key]})`);
-	});
+    Object.keys(breakpoints).forEach((key) => {
+        mqList[key] = window.matchMedia(`(min-width: ${breakpoints[key]})`);
+    });
 }
 
 export function addMQListener(bpList, cb) {
-	if (Array.isArray(bpList)) {
-		bpList.forEach((bp) => {
-			addBreakPointListener(bp, cb);
-		});
-	} else {
-		addBreakPointListener(bpList, cb);
-	}
+    if (Array.isArray(bpList)) {
+        bpList.forEach((bp) => {
+            addBreakPointListener(bp, cb);
+        });
+    } else {
+        addBreakPointListener(bpList, cb);
+    }
 }
 
 export function removeMQListener(bpList, cb) {
-	if (Array.isArray(bpList)) {
-		bpList.forEach((bp) => {
-			removeBreakPointListener(bp, cb);
-		});
-	} else {
-		removeBreakPointListener(bpList, cb);
-	}
+    if (Array.isArray(bpList)) {
+        bpList.forEach((bp) => {
+            removeBreakPointListener(bp, cb);
+        });
+    } else {
+        removeBreakPointListener(bpList, cb);
+    }
 }
 
 export function mqMatches(bp) {
-	return !!mqList[bp] && mqList[bp].matches;
+    return !!mqList[bp] && mqList[bp].matches;
 }
