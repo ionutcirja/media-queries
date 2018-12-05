4d65e2c50e9a229a5a05a319f2ac370f90adc5b6
Ionut Cirja <ionut.cirja@gmail.com>
2018-12-05 08:55:40 +0000

    plug in flow

--- src/index.js
+++ src/index.js
@@ -1,50 +1,58 @@
-const mqs = {};
+// @flow
 
-const addBPListener = (bp, cb) => {
-    if (mqs[bp]) {
-        mqs[bp].addListener(cb);
-    }
+type Queries = {
+  [key: string]: MediaQueryList,
 };
 
-const removeBPListener = (bp, cb) => {
-    if (mqs[bp]) {
-        mqs[bp].removeListener(cb);
-    }
+type Breakpoints = {
+  [key: string]: string,
 };
 
-const init = (bps) => {
-    Object.keys(bps).forEach((key) => {
-        mqs[key] = window.matchMedia(`(min-width: ${bps[key]})`);
+const mqs: Queries = {};
+
+const addBPListener = (bp: string, cb: Function) => {
+  if (mqs[bp]) {
+    mqs[bp].addListener(cb);
+  }
+};
+
+const removeBPListener = (bp: string, cb: Function) => {
+  if (mqs[bp]) {
+    mqs[bp].removeListener(cb);
+  }
+};
+
+const init = (bps: Breakpoints) => {
+  Object.keys(bps).forEach((key) => {
+    mqs[key] = window.matchMedia(`(min-width: ${bps[key]})`);
+  });
+};
+
+const addMQListener = (bps: Array<string> | string, cb: Function) => {
+  if (Array.isArray(bps)) {
+    bps.forEach((bp) => {
+      addBPListener(bp, cb);
     });
+  } else {
+    addBPListener(bps, cb);
+  }
 };
 
-const addMQListener = (bps, cb) => {
-    if (Array.isArray(bps)) {
-        bps.forEach((bp) => {
-            addBPListener(bp, cb);
-        });
-    } else {
-        addBPListener(bps, cb);
-    }
+const removeMQListener = (bps: Array<string> | string, cb: Function) => {
+  if (Array.isArray(bps)) {
+    bps.forEach((bp) => {
+      removeBPListener(bp, cb);
+    });
+  } else {
+    removeBPListener(bps, cb);
+  }
 };
 
-const removeMQListener = (bps, cb) => {
-    if (Array.isArray(bps)) {
-        bps.forEach((bp) => {
-            removeBPListener(bp, cb);
-        });
-    } else {
-        removeBPListener(bps, cb);
-    }
-};
-
-const matchesBP = (bp) => {
-    return !!mqs[bp] && mqs[bp].matches;
-};
+const matchesBP = (bp: string) => !!mqs[bp] && mqs[bp].matches;
 
 export default {
-    init,
-    addMQListener,
-    removeMQListener,
-    matchesBP,
+  init,
+  addMQListener,
+  removeMQListener,
+  matchesBP,
 };
